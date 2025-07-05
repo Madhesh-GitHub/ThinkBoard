5841a665aed71ba07a179bf0b120b4e7de17e332
Madhesh K <madheshk7239087@gmail.com>
2025-07-05 23:39:49 +0530

    create models -> Note.js

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,9 +1,25 @@
-export function getAllNotes(req, res){
-    res.status(200).send("You got 5 notes!");
+import Note from "../models/Note.js";
+
+export async function getAllNotes(req, res){
+    try {
+        const notes = await Note.find({})
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error in getAllNotes controller", error)
+        res.status(500).json({message: "Internal Server Error!"})
+    }
 }
 
-export function createNote(req, res){
-    res.status(201).json({message: "Note created successfully"});
+export async function createNote(req, res){
+    try {
+        const {title, content} = req.body;
+        console.log({title, content})
+        const newNote = new Note(({title, content}));
+        await newNote.save();
+        res.status(201).json({message: "Note created successfully!"});
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error!"});
+    }
 }
 
 export function updateNote(req, res){
