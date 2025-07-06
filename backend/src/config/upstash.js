ae3ad7cbb87507c04ca52b69e6f1fa1d143cb0e2
Madhesh K <madheshk7239087@gmail.com>
2025-07-06 16:38:39 +0530

    rate limiter successfully implemented

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,17 @@
+import pkg from '@upstash/ratelimit';
+const { Ratelimit } = pkg;
+
+import { Redis } from "@upstash/redis";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// create a rate limiter that allows 100 requests per minute
+
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(), 
+    limiter: Ratelimit.slidingWindow(5, "20 s"),
+});
+
+export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,34 @@ import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
-
+import rateLimiter from "./middleware/rateLimiter.js"
 
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001
 
-connectDB();
 
-app.use(express.json()) 
+// middleware
+app.use(express.json())    // this middleware will parse the json bodies: req.body
+app.use(rateLimiter);
+
+//our simple custom middleware
+// app.use((req, res, next) => {
+//     console.log(`Req method is ${req.method} & Req URL is ${req.url}`)
+//     next();
+// })
+
 app.use("/api/notes", notesRoutes)
 
-app.listen(PORT, ()=> {
+
+connectDB().then(()=>{
+    app.listen(PORT, ()=> {
     console.log("Server started on PORT:", PORT)
-})
+    })
+});
+
+
 
 
 // mongodb+srv://madheshk7239087:nDEVWjGrdDUDupEi@cluster0.uty9asl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
